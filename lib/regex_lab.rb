b8f6fe104a460a7f6f98f42a0ce1bbc102662d21
Amber Taranto <ambzzzz@gmail.com>
2019-06-25 05:02:09 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -7,11 +7,8 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-    count = 0
     if text.scan(/^un\w*ing/i)
-        count += 1
     end
-    count
 end
 
 def words_five_letters_long(text)
