fcc0b5ff15a178439923bb868d0716acec029ee5
Amber Taranto <ambzzzz@gmail.com>
2019-06-25 05:00:16 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -7,7 +7,9 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-    text.scan(/^un\w*ing/i)
+    if text.scan(/^un\w*ing/i)
+        text += 1
+    end
 end
 
 def words_five_letters_long(text)
