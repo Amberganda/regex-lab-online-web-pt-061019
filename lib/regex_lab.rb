dc6c2f11a13cf73d21d1b1ef5900b94bcf621579
Amber Taranto <ambzzzz@gmail.com>
2019-06-25 05:00:53 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -7,8 +7,9 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
+    count = 0
     if text.scan(/^un\w*ing/i)
-        text += 1
+        count += 1
     end
 end
 
